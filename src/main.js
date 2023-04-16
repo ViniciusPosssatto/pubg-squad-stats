f433c5b23482d1d466cc6ed0b9a989215dd28235
vinicius <vinehsp@gmail.com>
2023-04-16 10:52:43 -0300

    FEAT: add vuetify e snack bar component

--- src/main.js
+++ src/main.js
@@ -6,9 +6,20 @@ import router from './router'
 
 import './assets/main.css'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
-app.mount('#app')
+app.use(vuetify).mount('#app')
